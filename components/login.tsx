c42e9e2b4def597155a88921267f9cf31f33a7e7
Johny Junior <johnyjunior64@gmail.com>
2025-06-10 17:38:33 -0300

    component(login): created login component

---
+++ components/login.tsx
@@ -0,0 +1,41 @@
+'use client'
+import Avatar from '@/public/assets/login-avatar-icon.png'
+import loginCadeado from '@/public/assets/login-cadeado-icon.png'
+import loginUser from '@/public/assets/login-user-icon.png'
+import Image from 'next/image'
+import localFont from 'next/font/local'
+ 
+const CupCakes = localFont({
+  src: '../public/fonts/CupCakes.otf',
+})
+
+export function Login() {
+    return (
+        <section className={`${CupCakes.className} flex flex-col justify-center items-center w-80 bg-[#F9F4EA] p-6 rounded-[20px] shadow-[6px_6px_0_#000000] border border-black relative text-center`}>
+            <section className='w-20 h-20 rounded-full border-6 border-black mx-auto mb-4 bg-white flex items-end justify-center overflow-hidden'>
+                <Image src={Avatar} alt='Pequeno avatar' className="w-14 object-cover" />
+            </section>
+            <div className="mb-4 relative">
+                <div className="absolute left-0 top-0 flex items-center pl-2 h-full">
+                    <span className="flex justify-center items-center text-lg"><Image src={loginUser} alt="user-icon" className='w-5 h-5 object-contain' /></span>
+                </div>
+                <input
+                    placeholder="PRIMEIRO BEIJO"
+                    disabled
+                    type="text"
+                    className='placeholder-black w-full pl-10 py-2 rounded-md bg-white border-2 border-black shadow-[3px_3px_0_#FFD93D] font-bold tracking-widest text-center disabled' />
+            </div>
+            <div className="mb-4 relative">
+                <div className="absolute left-0 top-0 flex items-center pl-2 h-full">
+                    <span className="flex justify-center items-center text-lg"><Image src={loginCadeado} alt="user-icon" className='w-5 h-5 object-contain' /></span>
+                </div>
+                <input className='font-cup-cakes w-full pl-10 py-2 rounded-md bg-white border-2 border-black shadow-[3px_3px_0_#FFD93D] font-bold tracking-widest text-center text-black caret-black' />
+            </div>
+
+            <button
+                type='submit'
+                className="cursor-pointer bg-gradient-to-b from-yellow-300 to-orange-400 border-2 border-black px-6 py-2 rounded-full text-white font-bold shadow-[3px_3px_0_#000000] hover:scale-105 transition-transform"
+            >Login</button>
+        </section>
+    )
+}
